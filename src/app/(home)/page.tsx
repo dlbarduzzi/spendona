c25662d230b620ff6879710ee8c0382414c07ed2
Dylan Barduzzi <dlbarduzzi@gmail.com>
2026-03-31 14:05:29 -0500

    chore: setup eslint and update boilerplate

---
+++ src/app/(home)/page.tsx
@@ -0,0 +1,7 @@
+export default function Page() {
+  return (
+    <div className="p-4">
+      <div className="px-4 py-2.5 bg-blue-200 max-w-fit">Welcome to Spendona!</div>
+    </div>
+  )
+}
---
+++ src/lib/config.ts
@@ -0,0 +1,7 @@
+import { env } from "./env"
+
+export const siteConfig = {
+  url: env.APP_URL,
+  name: "spendona",
+  description: "Track subscriptions, monitor spending, and never miss a charge again. All in one place.",
+}
